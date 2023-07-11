3c8216cd8e8b016a62a175bdd61de50ea488061f
102guss <brown_321@live.com>
2023-07-11 13:13:13 -0600

    nueva
    si

---
+++ condicionales y estructurasControl.js
@@ -0,0 +1,12 @@
+//condicionales y estructuras de control
+
+//tramitación de INE
+
+var edad = 18;
+
+if (edad > 18){
+console.log('ya puede tramitar tu INE');
+}
+else{
+    console.log('aún no puedes tramitar tu INE');
+}
