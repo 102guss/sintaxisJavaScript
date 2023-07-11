82167342a80c6b8c7cb0983a40c94f12352117c0
102guss <brown_321@live.com>
2023-07-11 14:22:41 -0600

    Update condicionales y estructurasControl.js

--- condicionales y estructurasControl.js
+++ condicionales y estructurasControl.js
@@ -4,9 +4,47 @@
 
 var edad = 18;
 
-if (edad > 18){
-console.log('ya puede tramitar tu INE');
+if (edad >= 18){
+console.log('ya puedes tramitar tu INE');
 }
 else{
     console.log('aún no puedes tramitar tu INE');
 }
+//.........................................................
+//usando condicional triple
+
+var edad2 = 17;
+if(edad2==18){
+    console.log('es momento de  tramitar tu INE');
+    
+}
+
+else if(edad2>18){
+    console.log('puedes tramitar tu INE');
+}
+else{
+    console.log('aún no puedes tramitar tu INE');
+}
+//-----------------------------------------------------
+//l mismo pero ahora con funciones
+
+ function funcionTramitar(edad2){
+
+    if(edad2==18){
+        console.log('es momento de  tramitar tu INE');
+        
+    }
+    
+    else if(edad2>18){
+        console.log('puedes tramitar tu INE');
+    }
+    else{
+        console.log('aún no puedes tramitar tu INE');
+    }
+}
+
+//mandando llamar a la funcion con su respectivo parámetro
+//console.log(funcionTramitar(18));// para imprimir en pantalla
+var x;
+x = funcionTramitar(20);
+console.log(x);
