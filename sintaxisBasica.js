df735b2b0e3abc159fac703172378b6ad0e23f11
102guss <brown_321@live.com>
2023-07-11 12:40:21 -0600

    sintaxis
    primeros pasos

---
+++ sintaxisBasica.js
@@ -0,0 +1,34 @@
+//serie d ejercicios en java script
+
+console.log('hola mundo')// mensaje bienvenida
+//..............................................
+
+var a =  10;
+var b = 20;
+console.log(a+b);
+//...............................................
+
+function mostrarMensaje(){
+
+console.log("hola mundito");
+
+}
+mostrarMensaje();
+//...................................................
+function suma(){// funcion sin parametros
+    var a=10;
+    var b=20;
+    var result;
+    result = a+b;
+    return result;
+    }
+    
+    console.log(suma());
+//............................................................................    
+function suma( a,  b){// funcion con parametros
+
+return a + b;
+}
+
+console.log("la suma es: ", suma(3,5));
+//................................................................
